1454ba8df9a888843d6dfd7b37524ecbd09918a6
Liam <reckziegel.william@gmail.com>
2025-12-29 09:28:27 -0800

    amend

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,36 @@ const nextConfig: NextConfig = {
   // Externalize heavy packages to prevent serverless function bloat
   serverExternalPackages: [
     'sharp',
+    'three',
+    '@react-three/fiber',
+    '@react-three/drei', 
+    '@react-three/postprocessing',
+    '@react-three/rapier',
   ],
-  // Exclude heavy packages from output file tracing for API routes
+  // Exclude heavy packages from output file tracing
   outputFileTracingExcludes: {
-    '/api/*': [
+    '*': [
       './node_modules/three/**',
       './node_modules/@react-three/**',
       './node_modules/sharp/**',
+      './node_modules/@dimforge/**',
+      './node_modules/postprocessing/**',
     ],
+  },
+  // Custom webpack config to externalize heavy packages on server
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'sharp': 'commonjs sharp',
+        'three': 'commonjs three',
+        '@react-three/fiber': 'commonjs @react-three/fiber',
+        '@react-three/drei': 'commonjs @react-three/drei',
+        '@react-three/postprocessing': 'commonjs @react-three/postprocessing',
+        '@react-three/rapier': 'commonjs @react-three/rapier',
+      });
+    }
+    return config;
   },
 };
 
